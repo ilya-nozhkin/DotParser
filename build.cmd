b0a135dfa6067d1291c2505ac52fe8166806ea9d
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-07-17 18:52:01 +0300

    Added packaging

--- build.cmd
+++ build.cmd
@@ -4,4 +4,3 @@ cls
 dotnet build DotParser.sln /P:Configuration=Release
 dotnet test tests\DotParser.Tests\DotParser.Tests.fsproj /P:Configuration=Release
 dotnet pack src\DotParser\DotParser.fsproj /P:Configuration=Release
-
